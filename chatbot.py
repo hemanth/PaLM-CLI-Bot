edcd0f825862af85cca3851f0627891d9955e014
hemanth.hm <hemanth.hm@gmail.com>
2023-05-11 17:15:20 +0000

    feat: added generate_text functionality.

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,16 @@
+# rewrite the bleow 
 import os
 import google.generativeai as palm
 from halo import Halo
+
+defaults = {
+  'model': 'models/text-bison-001',
+  'temperature': 0.4,
+  'candidate_count': 1,
+  'top_k': 40,
+  'top_p': 0.95,
+  'max_output_tokens': 1024,
+}
 
 palm.configure(api_key=os.environ['PALM_API_KEY'])
 
@@ -13,4 +23,12 @@ while True:
     spinner.start()
     response = palm.chat(messages=input_msg)
     spinner.stop()
-    print(response.last + "\n")
+    if not response.last:
+        response = palm.generate_text(prompt=input_msg, **defaults)
+        print(response.result)
+        if not response.result:
+            print("CLAI> Sorry, I couldn't fetch a response for that.")
+            continue
+    else:
+        print(response.last)
+        continue
